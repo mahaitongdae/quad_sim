e7d5ca7fa9ce2469371ac3614b853f61ea37a1f1
mhtdesk <mhtmht47@gmail.com>
2024-02-09 23:21:37 -0500

    init commit

---
+++ quadrotor_control.py
@@ -0,0 +1,278 @@
+import numpy as np
+from numpy.linalg import norm
+from copy import deepcopy
+
+import gym
+from gym import spaces
+from quad_utils import *
+
+GRAV = 9.81
+
+# like raw motor control, but shifted such that a zero action
+# corresponds to the amount of thrust needed to hover.
+class ShiftedMotorControl(object):
+    def __init__(self, dynamics):
+        pass
+
+    def action_space(self, dynamics):
+        # make it so the zero action corresponds to hovering
+        low = -1.0 * np.ones(4)
+        high = (dynamics.thrust_to_weight - 1.0) * np.ones(4)
+        return spaces.Box(low, high, dtype=np.float32)
+
+    # modifies the dynamics in place.
+    def step(self, dynamics, action, dt):
+        action = (action + 1.0) / dynamics.thrust_to_weight
+        action[action < 0] = 0
+        action[action > 1] = 1
+        dynamics.step(action, dt)
+
+class RawControl(object):
+    def __init__(self, dynamics, zero_action_middle=True):
+        self.zero_action_middle = zero_action_middle
+        # print("RawControl: self.zero_action_middle", self.zero_action_middle)
+        self.action = None
+        self.step_func = self.step
+
+    def action_space(self, dynamics):
+        if not self.zero_action_middle:
+            # Range of actions 0 .. 1
+            self.low = np.zeros(4)
+            self.bias = 0
+            self.scale = 1.0
+        else:
+            # Range of actions -1 .. 1
+            self.low = -np.ones(4)
+            self.bias =  1.0
+            self.scale = 0.5
+        self.high = np.ones(4)
+        return spaces.Box(self.low, self.high, dtype=np.float32)
+
+    # modifies the dynamics in place.
+    def step(self, dynamics, action, goal, dt, observation=None):
+        action = self.scale * (action + self.bias)
+        action = np.clip(action, a_min=self.low, a_max=self.high)
+        dynamics.step(action, dt)
+        self.action = action.copy()
+
+    def step_tf(self, dynamics, action, goal, dt, observation=None):
+        # print('bias/scale: ', self.scale, self.bias)
+        action = self.scale * (action + self.bias)
+        action = np.clip(action, a_min=self.low, a_max=self.high)
+        dynamics.step(action, dt)
+        self.action = action.copy()
+
+class RawControl(object):
+    def __init__(self, dynamics, zero_action_middle=True, dim_mode="3D"):
+        self.zero_action_middle = zero_action_middle
+        # print("RawControl: self.zero_action_middle", self.zero_action_middle)
+        self.action = None
+        self.step_func = self.step
+
+    def action_space(self, dynamics):
+        if not self.zero_action_middle:
+            # Range of actions 0 .. 1
+            self.low = np.zeros(4)
+            self.bias = 0
+            self.scale = 1.0
+        else:
+            # Range of actions -1 .. 1
+            self.low = -np.ones(4)
+            self.bias =  1.0
+            self.scale = 0.5
+        self.high = np.ones(4)
+        return spaces.Box(self.low, self.high, dtype=np.float32)
+
+    # modifies the dynamics in place.
+    def step(self, dynamics, action, goal, dt, observation=None):
+        action = self.scale * (action + self.bias)
+        action = np.clip(action, a_min=self.low, a_max=self.high)
+        dynamics.step(action, dt)
+        self.action = action.copy()
+
+    def step_tf(self, dynamics, action, goal, dt, observation=None):
+        # print('bias/scale: ', self.scale, self.bias)
+        action = self.scale * (action + self.bias)
+        action = np.clip(action, a_min=self.low, a_max=self.high)
+        dynamics.step(action, dt)
+        self.action = action.copy()
+
+
+class VerticalControl(object):
+    def __init__(self, dynamics, zero_action_middle=True, dim_mode="3D"):
+        self.zero_action_middle = zero_action_middle
+
+        self.dim_mode = dim_mode
+        if self.dim_mode == '1D':
+            self.step = self.step1D
+        elif self.dim_mode == '3D':
+            self.step = self.step3D
+        else:
+            raise ValueError('QuadEnv: Unknown dimensionality mode %s' % self.dim_mode)
+        self.step_func = self.step
+
+
+    def action_space(self, dynamics):
+        if not self.zero_action_middle:
+            # Range of actions 0 .. 1
+            self.low = np.zeros(1)
+            self.bias = 0
+            self.scale = 1.0
+        else:
+            # Range of actions -1 .. 1
+            self.low = -np.ones(1)
+            self.bias =  1.0
+            self.scale = 0.5
+        self.high = np.ones(1)
+        return spaces.Box(self.low, self.high, dtype=np.float32)
+
+    # modifies the dynamics in place.
+    def step3D(self, dynamics, action, goal, dt, observation=None):
+        # print('action: ', action)
+        action = self.scale * (action + self.bias)
+        action = np.clip(action, a_min=self.low, a_max=self.high)
+        dynamics.step(np.array([action[0]]*4), dt)
+
+    # modifies the dynamics in place.
+    def step1D(self, dynamics, action, goal, dt, observation=None):
+        # print('action: ', action)
+        action = self.scale * (action + self.bias)
+        action = np.clip(action, a_min=self.low, a_max=self.high)
+        dynamics.step(np.array([action[0]]), dt)
+
+class VertPlaneControl(object):
+    def __init__(self, dynamics, zero_action_middle=True, dim_mode="3D"):
+        self.zero_action_middle = zero_action_middle
+
+        self.dim_mode = dim_mode
+        if self.dim_mode == '2D':
+            self.step = self.step2D
+        elif self.dim_mode == '3D':
+            self.step = self.step3D
+        else:
+            raise ValueError('QuadEnv: Unknown dimensionality mode %s' % self.dim_mode)
+        self.step_func = self.step
+
+    def action_space(self, dynamics):
+        if not self.zero_action_middle:
+            # Range of actions 0 .. 1
+            self.low = np.zeros(2)
+            self.bias = 0
+            self.scale = 1.0
+        else:
+            # Range of actions -1 .. 1
+            self.low = -np.ones(2)
+            self.bias =  1.0
+            self.scale = 0.5
+        self.high = np.ones(2)
+        return spaces.Box(self.low, self.high, dtype=np.float32)
+
+    # modifies the dynamics in place.
+    def step3D(self, dynamics, action, goal, dt, observation=None):
+        # print('action: ', action)
+        action = self.scale * (action + self.bias)
+        action = np.clip(action, a_min=self.low, a_max=self.high)
+        dynamics.step(np.array([action[0], action[0], action[1], action[1]]), dt)
+
+    # modifies the dynamics in place.
+    def step2D(self, dynamics, action, goal, dt, observation=None):
+        # print('action: ', action)
+        action = self.scale * (action + self.bias)
+        action = np.clip(action, a_min=self.low, a_max=self.high)
+        dynamics.step(np.array(action), dt)
+
+
+# jacobian of (acceleration magnitude, angular acceleration)
+#       w.r.t (normalized motor thrusts) in range [0, 1]
+def quadrotor_jacobian(dynamics):
+    torque = dynamics.thrust_max * dynamics.prop_crossproducts.T
+    torque[2,:] = dynamics.torque_max * dynamics.prop_ccw
+    thrust = dynamics.thrust_max * np.ones((1,4))
+    dw = (1.0 / dynamics.inertia)[:,None] * torque
+    dv = thrust / dynamics.mass
+    J = np.vstack([dv, dw])
+    J_cond = np.linalg.cond(J)
+    # assert J_cond < 100.0
+    if J_cond > 50:
+        print("WARN: Jacobian conditioning is high: ", J_cond)
+    return J
+
+
+# P-only linear controller on angular velocity.
+# direct (ignoring motor lag) control of thrust magnitude.
+class OmegaThrustControl(object):
+    def __init__(self, dynamics):
+        jacobian = quadrotor_jacobian(dynamics)
+        self.Jinv = np.linalg.inv(jacobian)
+
+    def action_space(self, dynamics):
+        circle_per_sec = 2 * np.pi
+        max_rp = 5 * circle_per_sec
+        max_yaw = 1 * circle_per_sec
+        min_g = -1.0
+        max_g = dynamics.thrust_to_weight - 1.0
+        low  = np.array([min_g, -max_rp, -max_rp, -max_yaw])
+        high = np.array([max_g,  max_rp,  max_rp,  max_yaw])
+        return spaces.Box(low, high, dtype=np.float32)
+
+    # modifies the dynamics in place.
+    def step(self, dynamics, action, dt):
+        kp = 5.0 # could be more aggressive
+        omega_err = dynamics.omega - action[1:]
+        dw_des = -kp * omega_err
+        acc_des = GRAV * (action[0] + 1.0)
+        des = np.append(acc_des, dw_des)
+        thrusts = np.matmul(self.Jinv, des)
+        thrusts[thrusts < 0] = 0
+        thrusts[thrusts > 1] = 1
+        dynamics.step(thrusts, dt)
+
+
+# TODO: this has not been tested well yet.
+class VelocityYawControl(object):
+    def __init__(self, dynamics):
+        jacobian = quadrotor_jacobian(dynamics)
+        self.Jinv = np.linalg.inv(jacobian)
+
+    def action_space(self, dynamics):
+        vmax = 20.0 # meters / sec
+        dymax = 4 * np.pi # radians / sec
+        high = np.array([vmax, vmax, vmax, dymax])
+        return spaces.Box(-high, high, dtype=np.float32)
+
+    def step(self, dynamics, action, dt):
+        # needs to be much bigger than in normal controller
+        # so the random initial actions in RL create some signal
+        kp_v = 5.0
+        kp_a, kd_a = 100.0, 50.0
+
+        e_v = dynamics.vel - action[:3]
+        acc_des = -kp_v * e_v + npa(0, 0, GRAV)
+
+        # rotation towards the ideal thrust direction
+        # see Mellinger and Kumar 2011
+        R = dynamics.rot
+        zb_des, _ = normalize(acc_des)
+        yb_des, _ = normalize(cross(zb_des, R[:,0]))
+        xb_des    = cross(yb_des, zb_des)
+        R_des = np.column_stack((xb_des, yb_des, zb_des))
+
+        def vee(R):
+            return np.array([R[2,1], R[0,2], R[1,0]])
+        e_R = 0.5 * vee(np.matmul(R_des.T, R) - np.matmul(R.T, R_des))
+        omega_des = np.array([0, 0, action[3]])
+        e_w = dynamics.omega - omega_des
+
+        dw_des = -kp_a * e_R - kd_a * e_w
+        # we want this acceleration, but we can only accelerate in one direction!
+        thrust_mag = np.dot(acc_des, dynamics.rot[:,2])
+
+        des = np.append(thrust_mag, dw_des)
+        thrusts = np.matmul(self.Jinv, des)
+        thrusts = np.clip(thrusts, a_min=0.0, a_max=1.0)
+        dynamics.step(thrusts, dt)
+
+
+# TODO:
+# class AttitudeControl,
+# refactor common parts of VelocityYaw and NonlinearPosition
