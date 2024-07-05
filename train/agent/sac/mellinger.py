ef1aff93cc0ae45b9d3ce0686607bca4fdd2910d
haitong_office <mhtmht47@gmail.com>
2024-07-05 19:10:27 -0400

    first ddpg

--- train/agent/sac/mellinger.py
+++ train/agent/sac/mellinger.py
@@ -33,28 +33,28 @@ class DifferentiableMellinger(nn.Module):
         self.MAX_RPM = max_rpm
         self.integral_error = torch.zeros([3, ])
         # XY positions
-        self.kp_xy = torch.nn.Parameter(torch.tensor(0.4, requires_grad=True))
-        self.ki_xy = torch.nn.Parameter(torch.tensor(0.05, requires_grad=True))
-        self.kd_xy = torch.nn.Parameter(torch.tensor(0.2, requires_grad=True))
+        self.kp_xy = 0.4
+        self.ki_xy = 0.05
+        self.kd_xy = 0.2
         # Z position
-        self.kp_z = torch.nn.Parameter(torch.tensor(1.25, requires_grad=True))
-        self.ki_z = torch.nn.Parameter(torch.tensor(0.05, requires_grad=True))
-        self.kd_z = torch.nn.Parameter(torch.tensor(0.4, requires_grad=True))
+        self.kp_z = 1.25
+        self.ki_z = 0.05
+        self.kd_z = 0.4
         # Attitude
-        self.kR_xy = torch.nn.Parameter(torch.tensor(70000., requires_grad=True))
-        self.kw_xy = torch.nn.Parameter(torch.tensor(0., requires_grad=True))
-        self.ki_m_xy = torch.nn.Parameter(torch.tensor(20000., requires_grad=True))
+        self.kR_xy = 70000.
+        self.kw_xy = 0.
+        self.ki_m_xy = 20000.
+        # Z Altitude
+        self.kR_z = 60000.
+        self.kw_z = 500.
+        self.ki_m_z = 12000.
 
-        self.kR_z = torch.nn.Parameter(torch.tensor(60000., requires_grad=True))
-        self.kw_z = torch.nn.Parameter(torch.tensor(500., requires_grad=True))
-        self.ki_m_z = torch.nn.Parameter(torch.tensor(12000., requires_grad=True))
-
-        self.P_COEFF_FOR = torch.stack([self.kp_xy, self.kp_xy, self.kp_z])
-        self.I_COEFF_FOR = torch.stack([self.ki_xy, self.ki_xy, self.ki_z])
-        self.D_COEFF_FOR = torch.stack([self.kd_xy, self.kd_xy, self.kd_z])
-        self.P_COEFF_TOR = torch.stack([self.kR_xy, self.kR_xy, self.kR_z])
-        self.I_COEFF_TOR = torch.stack([self.kw_xy, self.kw_xy, self.kw_z])
-        self.D_COEFF_TOR = torch.stack([self.ki_m_xy, self.ki_m_xy, self.ki_m_z])
+        self.P_COEFF_FOR = torch.nn.Parameter(torch.tensor([self.kp_xy, self.kp_xy, self.kp_z], requires_grad=True))
+        self.I_COEFF_FOR = torch.nn.Parameter(torch.tensor([self.ki_xy, self.ki_xy, self.ki_z], requires_grad=True))
+        self.D_COEFF_FOR = torch.nn.Parameter(torch.tensor([self.kd_xy, self.kd_xy, self.kd_z], requires_grad=True))
+        self.P_COEFF_TOR = torch.nn.Parameter(torch.tensor([self.kR_xy, self.kR_xy, self.kR_z], requires_grad=True))
+        self.I_COEFF_TOR = torch.nn.Parameter(torch.tensor([self.kw_xy, self.kw_xy, self.kw_z], requires_grad=True))
+        self.D_COEFF_TOR = torch.nn.Parameter(torch.tensor([self.ki_m_xy, self.ki_m_xy, self.ki_m_z], requires_grad=True))
 
         self.i_range_xy = 2.0
         self.i_range_z = 0.4
@@ -68,8 +68,22 @@ class DifferentiableMellinger(nn.Module):
             [.5, .5, -1],
             [.5, -.5, 1]
         ]).float()
+        self.goal = torch.tensor([0., 0., 1.])
+        self.target_x_c = torch.tensor([1., 0., 0.]) # assume target rpy always 0
+        self.gravity = torch.tensor([0, 0, self.GRAVITY])
         self.output_type = output
         self.reset()
+
+        def transpose(x):
+            return x.T
+        self.vec_transpose = torch.vmap(transpose)
+
+    def set_device(self, device):
+        self.MIXER_MATRIX = self.MIXER_MATRIX.to(device)
+        self.goal = self.goal.to(device)
+        self.target_x_c = self.target_x_c.to(device)
+        self.gravity = self.gravity.to(device)
+
 
     def quaternion_to_matrix(self, quaternions: torch.Tensor) -> torch.Tensor:
         """
@@ -194,8 +208,8 @@ class DifferentiableMellinger(nn.Module):
         #### Store the last roll, pitch, and yaw ###################
         self.last_rpy = torch.zeros(3)
         #### Initialized PID control variables #####################
-        self.last_pos_e = torch.zeros(3)
-        self.integral_pos_e = torch.zeros(3)
+        self.last_pos_e = torch.zeros(1, 3)
+        self.integral_pos_e = torch.zeros(1, 3)
         self.last_rpy_e = torch.zeros(3)
         self.integral_rpy_e = torch.zeros(3)
 
@@ -206,32 +220,35 @@ class DifferentiableMellinger(nn.Module):
         #### then 12: integral error of pos 3, diff error of pos 3, integral error of angle 3, diff error of angle 3
 
         # position control
-        pos_e = torch.tensor([0., 0., 1.]) - obs[0:3]
-        cur_quat = obs[3:7]
-        cur_rpy = obs[7:10]
-        cur_vel = obs[10:13]
-        intergral_pos_error = obs[16:19]
-        diff_pos_error = obs[19:22]
-        integral_rpy_error = obs[22:25]
-        diff_rpy_error = -1 * obs[25:28]
+        if len(obs.shape) == 1:
+            obs = obs.unsqueeze(0)
+
+        pos_e = self.goal - obs[:, 0:3]
+        cur_quat = obs[:, 3:7]
+        cur_rpy = obs[:, 7:10]
+        cur_vel = obs[:, 10:13]
+        integral_pos_error = obs[:, 16:19]
+        diff_pos_error = obs[:, 19:22]
+        integral_rpy_error = obs[:, 22:25]
+        diff_rpy_error = -1 * obs[:, 25:28]
         cur_rotation = self.quaternion_to_matrix(cur_quat)
         vel_e = - cur_vel
-        self.integral_pos_e = self.integral_pos_e + pos_e * 1 / 240
-        self.integral_pos_e = torch.clip(self.integral_pos_e, -2., 2.)
-
-        self.integral_pos_e[2] = torch.clip(self.integral_pos_e[2], -0.15, .15)
+        # self.integral_pos_e = self.integral_pos_e + pos_e * 1 / 240
+        # self.integral_pos_e = torch.clip(self.integral_pos_e, -2., 2.)
+        # self.integral_pos_e[:, 2] = torch.clip(self.integral_pos_e[:, 2], -0.15, .15)
         #### PID target thrust #####################################
         target_thrust = torch.multiply(self.P_COEFF_FOR, pos_e) \
-                        + torch.multiply(self.I_COEFF_FOR, self.integral_pos_e) \
-                        + torch.multiply(self.D_COEFF_FOR, vel_e) + torch.tensor([0, 0, self.GRAVITY]) # , device=self.de
-        scalar_thrust = torch.clamp(torch.inner(target_thrust, cur_rotation[:, 2]), 0, torch.inf)
-        thrust_pwm = (torch.sqrt(scalar_thrust / (4 * self.KF)) - self.PWM2RPM_CONST) / self.PWM2RPM_SCALE
-        # thrust_pwm = self.massThrust * scalar_thrust
-        target_z_ax = target_thrust / torch.norm(target_thrust)
-        target_x_c = torch.tensor([1., 0., 0.]) # assume target rpy always 0
-        target_y_ax = torch.cross(target_z_ax, target_x_c) / torch.norm(torch.cross(target_z_ax, target_x_c))
-        target_x_ax = torch.cross(target_y_ax, target_z_ax)
-        target_rotation = torch.vstack([target_x_ax, target_y_ax, target_z_ax]).T
+                        + torch.multiply(self.I_COEFF_FOR, integral_pos_error) \
+                        + torch.multiply(self.D_COEFF_FOR, vel_e) + self.gravity # , device=self.de
+        scalar_thrust = torch.clamp(torch.vmap(torch.inner)(target_thrust, cur_rotation[:, :, 2]), 0, torch.inf)
+        # thrust_pwm = (torch.sqrt(scalar_thrust / (4 * self.KF)) - self.PWM2RPM_CONST) / self.PWM2RPM_SCALE
+        thrust_pwm = self.massThrust * scalar_thrust
+        target_z_ax = F.normalize(target_thrust, dim=1)
+        # target_x_c = torch.tensor([1., 0., 0.]) # assume target rpy always 0
+        target_y_ax = F.normalize(torch.vmap(torch.cross, in_dims=(0, None))(target_z_ax, self.target_x_c), dim=1) # / torch.norm(torch.cross(target_z_ax, target_x_c))
+        target_x_ax = torch.vmap(torch.cross)(target_y_ax, target_z_ax)
+        target_rotation_transposed = torch.stack([target_x_ax, target_y_ax, target_z_ax],dim=1)
+        target_rotation = torch.permute(target_rotation_transposed, [0, 2, 1])
         #### Target rotation #######################################
         target_euler = self.matrix_to_euler_angles(target_rotation)
 
@@ -241,21 +258,24 @@ class DifferentiableMellinger(nn.Module):
         # target_quat = (Rotation.from_euler('XYZ', target_euler, degrees=False)).as_quat()
         # w, x, y, z = target_quat
         # target_rotation = (Rotation.from_quat([w, x, y, z])).as_matrix()
-        rot_matrix_e = torch.matmul((target_rotation.T), cur_rotation) - torch.matmul(cur_rotation.T, target_rotation)
-        rot_e = torch.stack([rot_matrix_e[2, 1], rot_matrix_e[0, 2], rot_matrix_e[1, 0]])
-        rpy_rates_e = self.target_rpy_rates - (cur_rpy - self.last_rpy) * 240
-        self.last_rpy = cur_rpy
-        self.integral_rpy_e = self.integral_rpy_e - rot_e / 240
-        self.integral_rpy_e = torch.clip(self.integral_rpy_e, -1500., 1500.)
-        self.integral_rpy_e[0:2] = torch.clip(self.integral_rpy_e[0:2], -1., 1.)
+
+
+        rot_matrix_e = (torch.matmul(self.vec_transpose(target_rotation), cur_rotation)
+                        - torch.matmul(self.vec_transpose(cur_rotation), target_rotation))
+        rot_e = torch.stack([rot_matrix_e[:, 2, 1], rot_matrix_e[:, 0, 2], rot_matrix_e[:, 1, 0]], dim=1)
+        self.rot_e = rot_e
+        # rpy_rates_e = self.target_rpy_rates - (cur_rpy - self.last_rpy) * 240
+        # self.last_rpy = cur_rpy
+        # self.integral_rpy_e = self.integral_rpy_e - rot_e / 240
+        # self.integral_rpy_e = torch.clip(self.integral_rpy_e, -1500., 1500.)
+        # self.integral_rpy_e[0:2] = torch.clip(self.integral_rpy_e[0:2], -1., 1.)
         #### PID target torques ####################################
         target_torques = - torch.multiply(self.P_COEFF_TOR, rot_e) \
                          + torch.multiply(self.D_COEFF_TOR, diff_rpy_error) \
                          + torch.multiply(self.I_COEFF_TOR, integral_rpy_error)
         target_torques = torch.clip(target_torques, -3200, 3200)
-        pwm = thrust_pwm + torch.matmul(self.MIXER_MATRIX, target_torques)
-        print(target_torques)
-        pwm = torch.clip(pwm, self.MIN_PWM, self.MAX_PWM)
+        pwm = thrust_pwm.unsqueeze(1) + torch.matmul(self.MIXER_MATRIX, target_torques.unsqueeze(2)).squeeze()
+        pwm = torch.clip(pwm, self.MIN_PWM, self.MAX_PWM) # .squeeze(dim=-1)
         if self.output_type == "pwm":
             return pwm / self.MAX_PWM
         elif self.output_type == "rpm":
@@ -293,16 +313,15 @@ def test_mellinger_controller():
     xyz = []
     import gymnasium as gym
     env = gym.make('hover-aviary-v0', gui=True, act=ActionType.PWM,
-                   initial_xyzs=np.array([[0.01, 0.01, 1.01]]),
-                   initial_rpys=np.array([[0.0, 0.0, 0.0]]),)
+                   )
     obs, info = env.reset()
     print(env.MAX_RPM)
     done = False
     policy = DifferentiableMellinger(max_rpm=env.MAX_RPM)
     pid = DSLPIDControl(drone_model=DroneModel.CF2X)
 
-    for i in range(500):
-        action = policy(torch.tensor(obs)).detach().numpy()
+    while not done:
+        action = policy(torch.tensor(obs)).detach().numpy()[0]
         # action = pid.computeControl(control_timestep= 1 / 240,
         #                             cur_pos= obs[:3],
         #                             cur_quat= obs[3:7],
@@ -312,13 +331,28 @@ def test_mellinger_controller():
         # action =  action / env.MAX_RPM
         obs, rew, terminated, truncated, info = env.step(action)
 
-        xyz.append(env.intergral_rpy_e)
+        xyz.append(np.hstack([obs[22:25], policy.integral_rpy_e.detach().numpy()]))
         done = terminated or truncated
         time.sleep(0.01)
 
     print(info)
     xyz = np.array(xyz)
     plt.plot(xyz)
-    # plt.legend(['x', 'y', 'z'])
+    plt.legend(['r', 'p', 'y', 'r2', 'p2', 'y2'])
     print(xyz.shape)
     plt.show()
+
+def test_batch_mellinger():
+    from gym_pybullet_drones.envs.single_agent_rl.HoverAviary import HoverAviary
+    import matplotlib.pyplot as plt
+    import time
+    import gymnasium as gym
+    env = gym.make('hover-aviary-v0', gui=False, act=ActionType.PWM,
+                   initial_xyzs=np.array([[0.01, 0.01, 1.01]]),
+                   initial_rpys=np.array([[0.0, 0.0, 0.0]]), )
+
+    obs, _ = env.reset()
+    obs1, _ , _, _, _ = env.step(env.action_space.sample())
+    batched_obs = torch.tensor([obs, obs1])
+    policy = DifferentiableMellinger()
+    print(policy(batched_obs))
