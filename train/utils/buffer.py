73d9510bbba53d27280960ce167e6c1a504404cf
Tianpeng <53916793+Tianpeng-Zhang@users.noreply.github.com>
2024-07-17 11:34:36 -0400

    add realbuffer

--- train/utils/buffer.py
+++ train/utils/buffer.py
@@ -10,6 +10,7 @@ import argparse
 import seaborn as sns
 import pandas as pd
 import os
+from scipy.spatial.transform import Rotation
 
 
 Batch = collections.namedtuple(
@@ -94,14 +95,14 @@ class RealDataBuffer(ReplayBuffer):
 		index = rawData['motor.m1req'] > 0
 		first_index = np.nonzero(index)[0][0]
 		# state
-		initial_x, initial_y = rawData['stateEstimateZ.x'][first_index] / 1000, rawData['stateEstimateZ.y'][first_index] / 1000
-		goal[0] = initial_x
-		goal[1] = initial_y
+		# initial_x, initial_y = rawData['stateEstimateZ.x'][first_index] / 1000, rawData['stateEstimateZ.y'][first_index] / 1000
+		# goal[0] = initial_x
+		# goal[1] = initial_y
 		pos_error = -1 * np.vstack([
-						 rawData['stateEstimateZ.x'][index] / 1000,
-						 rawData['stateEstimateZ.y'][index] / 1000,
-						 rawData['stateEstimateZ.z'][index] / 1000,
-						 ]).T + goal
+						 rawData['ctrlMel.pos_error_x'][index],
+						 rawData['ctrlMel.pos_error_y'][index],
+						 rawData['ctrlMel.pos_error_z'][index],
+						 ]).T#  + goal
 		integral_pos_error = np.zeros_like(pos_error)
 		diff_pos_error = np.zeros_like(pos_error)
 		for i in range(pos_error.shape[0]):
@@ -109,26 +110,29 @@ class RealDataBuffer(ReplayBuffer):
 		for i in range(1, pos_error.shape[0]):
 			diff_pos_error[i] = (pos_error[i] - pos_error[i - 1]) * 240
 		rpy = np.vstack([rawData['stabilizer.roll'][index],
-						 -1 * rawData['stabilizer.pitch'][index],		# all the logging is crazyflie yaw coordination
+						 rawData['stabilizer.pitch'][index],		# all the logging is crazyflie yaw coordination
 						 rawData['stabilizer.yaw'][index]]).T / 180. * np.pi
+		quat = np.zeros([rpy.shape[0], 4])
+		for i, angle in enumerate(rpy):
+			quat[i] = Rotation.from_euler('XYZ', np.multiply([1, -1, 1], angle)).as_quat()
 		vxyz = np.vstack([rawData['stateEstimate.vx'][index],
 						  rawData['stateEstimate.vy'][index],
 						  rawData['stateEstimate.vz'][index]]).T
 		rpy_rate = np.vstack([rawData['stateEstimateZ.rateRoll'][index],
-							  -1 * rawData['stateEstimateZ.ratePitch'][index],
+							  rawData['stateEstimateZ.ratePitch'][index],
 							  rawData['stateEstimateZ.rateYaw'][index]]).T / 1000.  # rate in milliradians
 		integral_rpy_error = np.vstack([rawData['ctrlMel.i_err_mx'][index],
-										-1 * rawData['ctrlMel.i_err_my'][index],
+										rawData['ctrlMel.i_err_my'][index],
 							  			rawData['ctrlMel.i_err_mz'][index]]).T  # rate in milliradians
 		ctrl = np.vstack([rawData['motor.m1req'][index] / 65535.,
 						  rawData['motor.m2req'][index] / 65535.,
 						  rawData['motor.m3req'][index] / 65535.,
 						  rawData['motor.m4req'][index] / 65535.]).T
-		state = np.hstack([pos_error, rpy, vxyz, rpy_rate,
+		state = np.hstack([pos_error, quat, rpy, vxyz, rpy_rate,
 						   integral_pos_error,			# integral
 						   -1 * vxyz,					# diff
 						   integral_rpy_error,			# integral
-						   -1 * rpy_rate,
+						   -1 * rpy_rate,				# integral
 						   ])
 		st = state[:-1]
 		at = ctrl[:-1]
@@ -137,13 +141,29 @@ class RealDataBuffer(ReplayBuffer):
 		return st, at, reward, stp1
 
 	def load_all_data(self, log_path):
+		sts, ats, rewards, stp1s = [], [], [], []
 		for log_file in os.listdir(log_path):
 			st, at, reward, stp1 = self.load_usd_data(os.path.join(log_path, log_file))
+			sts.append(st)
+			ats.append(at)
+			rewards.append(reward)
+			stp1s.append(stp1)
+		self.state = np.vstack(sts)
+		self.action = np.vstack(ats)
+		self.reward = np.hstack(rewards)[:, np.newaxis]
+		self.next_state = np.vstack(stp1s)
+		self.done = np.zeros_like(self.reward)
+		self.size = self.state.shape[0]
 
 
 def test_load_single_data():
 	buf = RealDataBuffer(state_dim=28, action_dim=4)
-	st, at, reward, stp1 = buf.load_usd_data('/media/naliseas-workstation/crazyflie/log26')
+	st, at, reward, stp1 = buf.load_usd_data('/media/naliseas-workstation/crazyflie/log27')
 	print(f"{st.shape} {at.shape} {reward.shape} {stp1.shape}")
+
+def test_replay():
+	buf = RealDataBuffer(state_dim=28, action_dim=4)
+	buf.load_all_data('/home/naliseas-workstation/Documents/haitong/sim_to_real/quad_sim/deploy/sample_log')
+	print(buf.sample(batch_size=256))
 
 
