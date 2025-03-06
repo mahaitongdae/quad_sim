21f978330d30301304c56b4502a333b518f58a86
haitong_office <mhtmht47@gmail.com>
2025-03-05 20:22:48 -0500

    update env

--- main_pyb.py
+++ main_pyb.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', type=str, default=device)
-    parser.add_argument("--dir", default='sac_raw_force_input', type=str)
+    parser.add_argument("--dir", default='sac_revise_no_delay', type=str)
     parser.add_argument("--alg", default="sac")  # Alg name (sac, feature_sac)
     parser.add_argument("--env", default="hover-aviary-v0")  # Environment name
     parser.add_argument("--seed", default=1, type=int)  # Sets Gym, PyTorch and Numpy seeds
